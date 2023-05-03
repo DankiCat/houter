9cf79a173a37ac1e26d26e3287b01fed9a483200
Cat <daniliukdaniil@gmail.com>
2023-05-03 23:47:08 +0300

    adding resources  and gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,28 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
 
-gulp.task('server', function() {
+// Задача для компиляции SCSS-файлов в CSS-файлы и их объединения
+function compileSass() {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+}
 
-    browserSync({
+// Задача для запуска локального сервера и отслеживания изменений в файлах
+function serve() {
+    browserSync.init({
         server: {
-            baseDir: "src"
+        baseDir: './src'
         }
     });
+    gulp.watch('src/sass/**/*.scss', compileSass);
+    gulp.watch('src/**/*.*').on('change', browserSync.reload);
+}
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
-
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
-
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+exports.default = gulp.series(compileSass, serve);
