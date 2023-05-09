152418b615965e4918f25d078b93edfca8e6e27c
Cat <daniliukdaniil@gmail.com>
2023-05-09 23:46:39 +0300

    some fixes and carousel update

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.review__items').slick({
+        arrows: false,
+        variableWidth: true,
+        centerMode: true,
+        initialSlide: 1,
+        dots: true,
+    });
+});
